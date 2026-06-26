4cbf62c9b7355b795d2b1eb085513aeee26247cf
Nguyen Trong Thuan <87819498+ntt0601zcoder@users.noreply.github.com>
2026-06-26 15:52:57 +0700

    33

---
+++ problems/0033-search-in-rotated-sorted-array/README.md
@@ -0,0 +1,11 @@
+# 33. Search In Rotated Sorted Array
+
+- Difficulty: ?
+- Link: https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+## Approach
+
+_TODO: jot down the idea, time/space complexity, and any edge cases._
+
+- Time:  O(?)
+- Space: O(?)
---
+++ problems/0033-search-in-rotated-sorted-array/solution.go
@@ -0,0 +1,30 @@
+// Package searchinrotatedsortedarray solves LeetCode 33. Search In Rotated Sorted Array.
+// https://leetcode.com/problems/search-in-rotated-sorted-array/
+package searchinrotatedsortedarray
+
+func search(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+
+	for left <= right {
+		mid := left + (right-left)/2
+
+		if nums[mid] == target {
+			return mid
+		}
+
+		if nums[mid] >= nums[left] {
+			if target >= nums[left] && target < nums[mid] {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		} else {
+			if target > nums[mid] && target <= nums[right] {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		}
+	}
+	return -1
+}
