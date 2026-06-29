49b0bd9286ebe0da9b1415b22c47ef603a6114d0
Nguyen Trong Thuan <87819498+ntt0601zcoder@users.noreply.github.com>
2026-06-29 16:27:57 +0700

    4

---
+++ problems/0004-median-of-two-sorted-arrays/README.md
@@ -0,0 +1,11 @@
+# 4. Median Of Two Sorted Arrays
+
+- Difficulty: ?
+- Link: https://leetcode.com/problems/median-of-two-sorted-arrays/
+
+## Approach
+
+_TODO: jot down the idea, time/space complexity, and any edge cases._
+
+- Time:  O(?)
+- Space: O(?)
---
+++ problems/0004-median-of-two-sorted-arrays/solution.go
@@ -0,0 +1,69 @@
+// Package medianoftwosortedarrays solves LeetCode 4. Median Of Two Sorted Arrays.
+// https://leetcode.com/problems/median-of-two-sorted-arrays/
+package medianoftwosortedarrays
+
+import (
+	"math"
+	"slices"
+)
+
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	short, long := nums1, nums2
+	if len(short) > len(long) {
+		short, long = long, short
+	}
+
+	shortLen, longLen := len(short), len(long)
+	totalLen := shortLen + longLen
+	leftHalfLen := (totalLen + 1) / 2
+
+	lo, hi := 0, shortLen
+	for lo <= hi {
+		shortCut := (lo + hi) / 2
+		longCut := leftHalfLen - shortCut
+
+		shortLeft, shortRight := math.MinInt, math.MaxInt
+		if shortCut > 0 {
+			shortLeft = short[shortCut-1]
+		}
+		if shortCut < shortLen {
+			shortRight = short[shortCut]
+		}
+
+		longLeft, longRight := math.MinInt, math.MaxInt
+		if longCut > 0 {
+			longLeft = long[longCut-1]
+		}
+		if longCut < longLen {
+			longRight = long[longCut]
+		}
+
+		switch {
+		case shortLeft > longRight:
+			hi = shortCut - 1
+		case longLeft > shortRight:
+			lo = shortCut + 1
+		default:
+			maxLeft := max(shortLeft, longLeft)
+			if totalLen%2 == 1 {
+				return float64(maxLeft)
+			}
+			minRight := min(shortRight, longRight)
+			return float64(maxLeft+minRight) / 2
+		}
+	}
+	return 0
+}
+
+func findMedianSortedArraysSort(nums1 []int, nums2 []int) float64 {
+	nums := slices.Concat(nums1, nums2)
+	slices.Sort(nums)
+
+	n := len(nums)
+
+	if n%2 == 0 {
+		return (float64(nums[n/2]) + float64(nums[n/2-1])) / 2
+	} else {
+		return float64(nums[n/2])
+	}
+}
